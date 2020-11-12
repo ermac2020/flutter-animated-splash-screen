fd93c8bfe8c24f2fba886753b2d45a799be0a560
ermac2020 <66848737+ermac2020@users.noreply.github.com>
2020-11-11 20:50:04 -0600

    .

--- lib/animation_screen.dart
+++ lib/animation_screen.dart
@@ -3,9 +3,7 @@ import 'hole_painter.dart';
 import 'staggered_raindrop_animation.dart';
 
 class AnimationScreen extends StatefulWidget {
-  AnimationScreen({
-    this.color
-  });
+  AnimationScreen({this.color});
 
   final Color color;
 
@@ -13,7 +11,8 @@ class AnimationScreen extends StatefulWidget {
   _AnimationScreenState createState() => _AnimationScreenState();
 }
 
-class _AnimationScreenState extends State<AnimationScreen> with SingleTickerProviderStateMixin {
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
   Size size = Size.zero;
   AnimationController _controller;
   StaggeredRaindropAnimation _animation;
@@ -22,7 +21,7 @@ class _AnimationScreenState extends State<AnimationScreen> with SingleTickerProv
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 3000),
+      duration: const Duration(seconds: 1),
       vsync: this,
     );
     _animation = StaggeredRaindropAnimation(_controller);
@@ -43,48 +42,34 @@ class _AnimationScreenState extends State<AnimationScreen> with SingleTickerProv
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
+    return Stack(children: [
+      Container(
           width: double.infinity,
           height: double.infinity,
           child: CustomPaint(
-            painter: HolePainter(
-              color: widget.color,
-              holeSize: _animation.holeSize.value * size.width
-            )
-          )
-        ),
-        Positioned(
+              painter: HolePainter(
+                  color: widget.color,
+                  holeSize: _animation.holeSize.value * size.width))),
+      Positioned(
           top: _animation.dropPosition.value * size.height,
           left: size.width / 2 - _animation.dropSize.value / 2,
           child: SizedBox(
-            width: _animation.dropSize.value,
-            height: _animation.dropSize.value,
-            child: CustomPaint(
-              painter: DropPainter(
-                visible: _animation.dropVisible.value
-              ),
-          )
-          )
-        ),
-        Padding(
+              width: _animation.dropSize.value,
+              height: _animation.dropSize.value,
+              child: CustomPaint(
+                painter: DropPainter(visible: _animation.dropVisible.value),
+              ))),
+      Padding(
           padding: EdgeInsets.only(bottom: 32),
           child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Opacity(
-              opacity: _animation.textOpacity.value,
-              child: Text(
-                'Raindrop Software',
-                style: TextStyle(
-                    color: Colors.white, fontSize: 32
-                ),
-              )
-            )
-          )
-        )
-      ]
-    );
+              alignment: Alignment.bottomCenter,
+              child: Opacity(
+                  opacity: _animation.textOpacity.value,
+                  child: Text(
+                    'Raindrop Software',
+                    style: TextStyle(color: Colors.white, fontSize: 32),
+                  ))))
+    ]);
   }
 
   @override
@@ -95,9 +80,7 @@ class _AnimationScreenState extends State<AnimationScreen> with SingleTickerProv
 }
 
 class DropPainter extends CustomPainter {
-  DropPainter({
-    this.visible = true
-  });
+  DropPainter({this.visible = true});
 
   bool visible;
 
@@ -111,8 +94,7 @@ class DropPainter extends CustomPainter {
     path.moveTo(size.width / 2, 0);
     path.quadraticBezierTo(0, size.height * 0.8, size.width / 2, size.height);
     path.quadraticBezierTo(size.width, size.height * 0.8, size.width / 2, 0);
-    canvas.drawPath(path, Paint()
-      ..color = Colors.white);
+    canvas.drawPath(path, Paint()..color = Colors.white);
   }
 
   @override
